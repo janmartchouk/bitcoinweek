d7ec2621708488e32e0d8f9485898e2e6310e712
najan <jan@martchouk.com>
2020-05-02 23:31:27 +0200

    Design 2

--- script.js
+++ script.js
@@ -23,3 +23,18 @@ function toggleMenu() {
         openMenu();
     }
 }
+
+// window.addEventListener('scroll', function() {
+//     var scrollTop = window.pageYOffset || window.scrollTop;
+//     var scrollPercent = scrollTop/100 || 0;
+    
+//     if(scrollPercent > 0) {
+//         document.querySelector('.nav').style.top = '10rem'
+//     } else {
+//         document.querySelector('.nav').style.top = '0';
+//         setTimeout(function() {
+//             document.querySelector('.nav').scrollIntoView();
+//         }, 100)
+//     }
+
+//   });
