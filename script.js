0899110cd806c13582a50089289da6901ddc5469
najan <jan@martchouk.com>
2020-05-03 11:37:55 +0200

    New Sponsors Logos, Location and Date

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var menuOpen = false
 
 function openMenu() {
     document.querySelector('.mob__menu').style.width = "100vw";
-    document.querySelector('body').style.overflow = "hidden";
+    // document.querySelector('body').style.overflow = "hidden";
     document.querySelector('.mob__btn').classList.toggle('mdi-menu');
     document.querySelector('.mob__btn').classList.toggle('mdi-close');
     menuOpen = true; 
@@ -10,7 +10,7 @@ function openMenu() {
 
 function closeMenu() {
     document.querySelector('.mob__menu').style.width = "0";
-    document.querySelector('body').style.overflow = "visible";
+    // document.querySelector('body').style.overflow = "visible";
     document.querySelector('.mob__btn').classList.toggle('mdi-menu');
     document.querySelector('.mob__btn').classList.toggle('mdi-close');
     menuOpen = false;
